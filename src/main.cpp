1afb637ce520d408cdd30fbc421f2683ee980b38
unknown <mohsen.samadani@gmail.com>
2018-01-02 15:05:50 -0500

    parameters adjusted.

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;  // steering controller
   // TODO: Initialize the pid variable.
-  pid.Init(0.15, 0.0, 5.0);
+  pid.Init(0.5, 0.0, 5.0);
 
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
@@ -51,7 +51,7 @@ int main()
           // j[1] is the data JSON object
           double cte = std::stod(j[1]["cte"].get<std::string>());
           //double speed = std::stod(j[1]["speed"].get<std::string>());
-          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+          //double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
           double throttle;
           /*
