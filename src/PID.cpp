3c75053a60647c2a04efe73007188c7b652a7bdc
unknown <mohsen.samadani@gmail.com>
2018-01-02 13:37:52 -0500

    First commit.

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,26 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+
+    Kp = Kp;
+    Ki = Ki;
+    Kd = Kd;
+
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
+
 }
 
 void PID::UpdateError(double cte) {
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
+
 }
 
 double PID::TotalError() {
+
+    return -(p_error * Kp + i_error * Ki + d_error * Kd);
 }
 
