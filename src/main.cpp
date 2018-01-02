ba8b6c20a085dc62b8dcaa0a60fa2e577ffdb27a
unknown <mohsen.samadani@gmail.com>
2018-01-02 15:07:14 -0500

    parameters adjusted.

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;  // steering controller
   // TODO: Initialize the pid variable.
-  pid.Init(0.5, 0.0, 5.0);
+  pid.Init(0.05, 0.0, 2.0);
 
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
@@ -53,7 +53,7 @@ int main()
           //double speed = std::stod(j[1]["speed"].get<std::string>());
           //double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
-          double throttle;
+
           /*
           * TODO: Calcuate steering value here, remember the steering value is
           * [-1, 1].
